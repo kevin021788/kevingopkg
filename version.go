2e31cef599639649530c5904d2f207e9491b4d13
kevin <kevin0217@126.com>
2021-11-11 11:31:01 +0800

    edit

--- version.go
+++ version.go
@@ -1,3 +1,17 @@
 package kevingopkg
 
-const VERSION = 1.1
+const VERSION=1.1
+
+const name ="kevin gopkg"
+
+func printName()  {
+	fmt.Println(name)
+}
+
+func PrintName()  {
+	fmt.Println(name)
+}
+
+func init() {
+	fmt.Println("init")
+}
